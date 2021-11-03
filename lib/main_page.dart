7cd223d1fa253a7085e0194e9cd0161622994935
Stanley Mesa <83571092+stanleymesa@users.noreply.github.com>
2021-11-03 20:53:57 +0700

    tabbar

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -19,12 +19,19 @@ class _MainPageState extends State<MainPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
           flexibleSpace: Container(
               decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/pattern.png"),
+                fit: BoxFit.cover,
+                colorFilter:
+                    ColorFilter.mode(Colors.black26, BlendMode.dstATop)),
             gradient: LinearGradient(
-                colors: [Color(0xff130F7F), Color(0xffFA8713)],
+                colors: [Color(0xfff70ad4), Color(0xffff8aee)],
                 begin: FractionalOffset.topLeft,
                 end: FractionalOffset.bottomRight),
           )),
@@ -42,56 +49,104 @@ class _MainPageState extends State<MainPage> {
           leading: Icon(
             Icons.person,
             color: Colors.white,
-          )),
-      body: Container(
-        margin: EdgeInsets.all(16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          ),
+          bottom: TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(Icons.home),
+                text: "Home",
+              ),
+              Tab(
+                icon: Icon(Icons.edit),
+                text: "Design",
+              ),
+              Tab(
+                icon: Icon(Icons.cake),
+                text: "Ready",
+              ),
+              Tab(
+                icon: Icon(Icons.add_box),
+                text: "Marmer",
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
           children: [
-            Card(
-              elevation: 3,
-              child: Row(
+            Container(
+              margin: EdgeInsets.all(16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Container(
-                    margin: EdgeInsets.fromLTRB(16, 16, 16, 16),
-                    child: Column(
-                      children: [Icon(Icons.person_add), Text("Add Person")],
+                  Card(
+                    elevation: 3,
+                    child: Row(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.fromLTRB(16, 16, 16, 16),
+                          child: Column(
+                            children: [
+                              Icon(Icons.person_add),
+                              Text("Add Person")
+                            ],
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.fromLTRB(0, 16, 16, 16),
+                          child: Column(
+                            children: [
+                              Icon(Icons.person_add),
+                              Text("Add Person")
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  Container(
-                    margin: EdgeInsets.fromLTRB(0, 16, 16, 16),
-                    child: Column(
-                      children: [Icon(Icons.person_add), Text("Add Person")],
-                    ),
+                  TextField(
+                    controller: controller,
+                    decoration: InputDecoration(
+                        labelText: "Masukkan Text",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10))),
+                  ),
+                  Text(showText),
+                  ElevatedButton(
+                    onPressed: () {
+                      showText = controller.text;
+                      setState(() {});
+                    },
+                    child: Text("Show Text"),
+                    style: ElevatedButton.styleFrom(primary: Colors.amber),
+                  ),
+                  ElevatedButton(
+                    child: Text("To Second Page"),
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) {
+                        return SecondPage();
+                      }));
+                    },
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.teal, onPrimary: Colors.white),
                   ),
                 ],
               ),
             ),
-            TextField(
-              controller: controller,
-              decoration: InputDecoration(
-                  labelText: "Masukkan Text",
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10))),
+            Container(
+              child: Center(
+                child: Text("Design Your Cake"),
+              ),
             ),
-            Text(showText),
-            ElevatedButton(
-              onPressed: () {
-                showText = controller.text;
-                setState(() {});
-              },
-              child: Text("Show Text"),
-              style: ElevatedButton.styleFrom(primary: Colors.amber),
+            Container(
+              child: Center(
+                child: Text("Tart Ready"),
+              ),
             ),
-            ElevatedButton(
-              child: Text("To Second Page"),
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return SecondPage();
-                }));
-              },
-              style: ElevatedButton.styleFrom(
-                  primary: Colors.teal, onPrimary: Colors.white),
+            Container(
+              child: Center(
+                child: Text("Kue Marmer"),
+              ),
             ),
           ],
         ),
